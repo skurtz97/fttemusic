51c38cddb9a11f5ed49612d632412480bfc56103
skurtz97 <seankurtz75@gmail.com>
2022-05-08 22:47:54 -0500

    fix margin

--- src/routes/songs/_song.svelte
+++ src/routes/songs/_song.svelte
@@ -438,5 +438,12 @@
       margin-bottom: 0;
       padding-bottom: 15%;
     }
+    .embed-container iframe {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
   }
 </style>
