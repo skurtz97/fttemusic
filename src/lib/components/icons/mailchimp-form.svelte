4033753746f90def2978847c6f69c7a0c6babceb
skurtz97 <seankurtz75@gmail.com>
2022-05-15 07:32:06 -0500

    forms

---
+++ src/lib/components/icons/mailchimp-form.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let nameVal = "";
+  let emailVal = "";
+</script>
+
+<form>
+  <label for="name">First Name (Required)</label>
+  <input name="name" placeholder="Name" required={true} bind:value={nameVal} />
+  <label for="email">Email Address (Required)</label>
+  <input name="email" placeholder="Email" required={true} bind:value={emailVal} />
+  <button class="btn">Sign up</button>
+</form>
---
+++ src/routes/thank-you.svelte
@@ -0,0 +1,44 @@
+<div class="container">
+  <h1>THANK YOU!</h1>
+  <h3>You should receive an email with a link to unreleased bonus content.</h3>
+
+  <p>
+    What an encouragement to have you with us at <b>Friends To The End!</b> We’ve been charting off
+    to infinity and beyond, with your continued support. Be sure to hear our recent releases
+    <b>Where’d You Put The Baby?</b>
+    and <b>A World Of Troubles</b> at our <a href="/songs">our Songs page</a> or any digital streaming
+    platform (DSP) like Apple Music, Spotify or Amazon Music.
+  </p>
+  <p>
+    To show our appreciation, have yourself an advance screening of our unreleased new music video
+    for <b>ROBOT ODDiTY</b>, coming to all the DSPs on June 1, 2022.
+  </p>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 80ch;
+    margin: 2rem auto;
+  }
+  h1 {
+    width: 100%;
+    text-align: left;
+    margin-bottom: 0;
+  }
+  h3::before {
+    content: "";
+    border-top: 1px gray solid;
+    width: 1em;
+    display: block;
+    padding: 0 0 15px 0;
+  }
+
+  @media (max-width: 1000px) {
+    .container {
+      max-width: 95%;
+    }
+  }
+</style>
