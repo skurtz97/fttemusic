1d4bd350112f393f39b59cbd730526d697732c2e
skurtz97 <seankurtz75@gmail.com>
2022-05-17 08:01:10 -0500

    added redirect page to genredetector.com

---
+++ src/routes/genredetector.svelte
@@ -0,0 +1,11 @@
+<svelte:window href="https://genredetector.com" />
+
+<svelte:head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="refresh" content="1;url=https://genredetector.com" />
+  <title>Redirect</title>
+</svelte:head>
+
+<p>
+  If you are not redirected automatically, follow the <a href="https://genredetector.com">link</a>
+</p>
