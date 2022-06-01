aa8ca66607f1d9083817c7d7711d9511121dd35e
tkurtz75 <thom_kurtz@hotmail.com>
2022-06-01 00:09:15 -0500

    looks like something is staged for mailing-list and thank-you pages

--- src/routes/thank-you.svelte
+++ src/routes/thank-you.svelte
@@ -14,7 +14,7 @@
     <iframe
       width="560"
       height="315"
-      src="https://www.youtube.com/embed/-phPAiOZCUE"
+      src="https://www.youtube-nocookie.com/embed/-phPAiOZCUE?origin=https://friendstotheend.net"
       title="YouTube video player"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
